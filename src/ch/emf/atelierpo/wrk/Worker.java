1cbb3aeec4e4ae7fb27afa7e7d6b22d961869e24
Siwan Khalil <siwan.khalil@studentfr.ch>
2024-04-17 09:04:35 +0200

    javadoc de la méthode immatriculation faite

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,7 +6,16 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+   /**
+     *
+     * Vérifie si un numéro de plaque de voiture suisse est valide.
+     *
+     * @param canton le canton de la plaque (doit être écrit en 2 lettres de A-Z
+     * en majuscule)
+     * @param numeroDePlaque le numéro de la plaque (constitué uniquement de
+     * nombres entiers)
+     * @return true si le numéro de plaque est valide, false sinon
+     */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
